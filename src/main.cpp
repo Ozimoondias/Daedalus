5c62b0dd7d513d07886c0691f85be2a084e1ca03
theo <theo.goetschy@gmail.com>
2020-04-29 00:09:58 +0200

    add find sfml and first window

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,23 @@
 **
 */
 
-int main(void)
+#include	<SFML/Graphics.hpp>
+
+int	main(void)
 {
+  sf::RenderWindow	window(sf::VideoMode(800, 600),
+			       "Daedalus");
+  sf::Event		event;
+
+  while (window.isOpen())
+    {
+      while (window.pollEvent(event))
+	if (event.type == sf::Event::Closed)
+	  window.close();
+
+      window.clear();
+      window.display();
+    }
+
   return 0;
 }
